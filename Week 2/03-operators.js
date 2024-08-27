44f989e2c3ab081222cca79112ec0a165f67ce40
Mark Dapula <dapulamarkchristian15@gmail.com>
2024-08-27 23:09:48 +0800

    added javascript lessons and finished exercise

---
+++ Week 2/03-operators.js
@@ -0,0 +1,90 @@
+// Arithmetic
+let num1 = 20;
+let num2 = 5;
+
+console.log("Addition (+):", num1 + num2);
+console.log("Substraction (-):", num1 - num2);
+console.log("Multiplication (*):", num1 * num2);
+console.log("Division (/):", num1 / num2);
+console.log("Modulo (%):", num1 % num2);
+console.log("Exponent (**):", num1 ** num2);
+
+// PE|MD|AS
+console.log("Mixed operations:", (5 + 3) * 2);
+
+// String Concatenation
+console.log("Hello" + " " + "World!");
+
+let greeting = "What's up";
+let myName = "Mr. " + 11;
+console.log(typeof myName);
+console.log(greeting + ", " + myName + "?");
+
+// Comparison Operators
+
+// Loose Equal to '==' : Checks if two values are equal
+console.log("Equal to (==):", 5 == 5);
+console.log("Equal to (==):", 5 == '5');
+
+// Strict Equal to '===': Values and Data Types are the same
+console.log("Equal to (===):", 5 === 5);
+console.log("Equal to (===):", 5 === '5');
+
+// Loose Not Equal to '!=' : Checks if two values are not equal
+console.log("Not Equal to (!=):", 5 != 5);
+console.log("Not Equal to (!=):", 5 != '5');
+
+// Strict Not Equal to '!==': Values and Data Types are not the same
+console.log("Not Equal to (!==):", 5 !== 5);
+console.log("Not Equal to (!==):", 5 !== '5');
+
+// Comparison Operators
+console.log("Greater Than (>):", 5 > 3);
+console.log("Greater Than or Equal To (>=):", 5 >= 3);
+console.log("Less Than (>):", 3 < 5);
+console.log("Less Than or Equal To (>=):", 3 <= 5);
+
+// Logical Operators
+/*
+    AND - &&
+    OR - ||
+    NOT - !
+*/
+
+let sunny = true;
+let warm = false;
+
+// AND: both sides are true == true
+console.log("Is it sunny AND warm?", sunny && warm); // False (T && F)
+
+// OR: at least one side is true == true
+console.log("Is it sunny OR warm?", sunny || warm); // True (T || F)
+
+// NOT: Opposite
+console.log("NOT sunny?", !sunny); // !true == false
+
+// Assignment Operators
+
+//Assigning values to variables (=)
+let num3 = 10;
+let num4 = 5;
+
+// Addition Assignment '+='
+// Computation: 10 + 5 = 15
+num3 += num4;
+console.log("The new value of a is:", num3);
+
+// Subtraction Assignment '-='
+// Computation: 15 - 5 = 10
+num3 -= num4;
+console.log("The new value of a is:", num3);
+
+// Multiplication Assignment '*='
+// Computation: 10 * 5 = 50
+num3 *= num4;
+console.log("The new value of a is:", num3);
+
+// Division Assignment '/='
+// Computation: 50 / 5 = 10
+num3 /= num4;
+console.log("The new value of a is:", num3);
---
+++ Week 2/Exercises/exercise-1.js
@@ -0,0 +1,66 @@
+// Exercise 1
+
+/* let score = '';
+while (score == '') {
+    score = prompt("Enter your score!");
+
+        if (score == '') {
+            alert("Please enter your score!");
+        } else if (score > 100 || score < 0) {
+            console.log("Please enter a valid score");
+        } else if (score >= 90 && score <= 100) {
+            console.log("Congratulations! You excel and your grade is A!");
+        } else if (score >= 80 && score < 90 ) {
+            console.log("Congratulations! You passed and your grade is B!");
+        } else if (score >= 70 && score < 80 ) {
+            console.log("Your grade is C!");
+        } else if (score >= 60 && score < 70 ) {
+            console.log("Your grade is D!");
+        } else {
+            console.log("Better luck next time! Your grade is F!");
+        } 
+}
+*/
+
+
+// Exercise 2
+
+/*let start = prompt("Enter starting number:")
+let end = prompt("Enter ending number:")
+
+for (let i = start; i <= end; i++) {
+    console.log(i);
+}
+*/
+
+
+// Exercise 3
+/*
+function power(base, exponent) {
+    let result = 1;
+
+    for (let i = 0; i < exponent; i++) {
+        result *= base;
+    }
+
+    return result;
+}
+
+console.log(power(5, 8));
+
+*/
+
+// Exercise 4
+
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+    
